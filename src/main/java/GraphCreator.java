5430affedef077f960912db5a78e5888ca76766f
Gabe Perez <gbpz321@gmail.com>
2021-11-28 20:20:44 -0600

    chore: simple demo

--- src/main/java/GraphCreator.java
+++ src/main/java/GraphCreator.java
@@ -1,4 +1,6 @@
 import algorithms.AntColHeuristic;
+import algorithms.TabucolHeuristic;
+import formatter.GraphOutputFormatter;
 import graph.definition.GraphDefinition;
 import graph.solution.GraphSolution;
 import reader.DimacsReader;
@@ -10,45 +12,17 @@ public class GraphCreator {
 
     public static void main(String[] args) throws IOException {
         GraphReader reader = new DimacsReader();
-        String name = "graphs/dsjc125.1.col";
+        String name = "graphs\\dsjc125.1.col";
         GraphDefinition testGraph = reader.getGraph(GraphCreator.class.getClassLoader().getResourceAsStream(name), name);
 
-        AntColHeuristic antColHeuristic = new AntColHeuristic(testGraph, false);
-        GraphSolution solution = antColHeuristic.getColoring();
+        TabucolHeuristic tabucolHeuristic = new TabucolHeuristic(testGraph);
+        GraphSolution solution = tabucolHeuristic.getColoring();
         System.out.println(solution.getK());
-        System.out.println(antColHeuristic.getName());
-
-//        List<ColoringHeuristic> heuristics = Arrays.asList(new PartialColHeuristic(testGraph), new HCDHeuristic(testGraph.getGraphWrapper()), new TabucolHeuristic(testGraph));
-//        List<ColoringHeuristic> heuristics = Collections.singletonList(new MetropolisHeuristic(testGraph));
-//        for(ColoringHeuristic heuristic : heuristics) {
-//            GraphSolution solution = heuristic.getColoring();
-//            System.out.println(solution.getK());
-//            System.out.println(heuristic.getName());
-//            System.out.println(sat(testGraph, solution.getColoring()));
-//        }
-
-//        SimpleGeneticSubroutine simpleGeneticSubroutine = new SimpleGeneticSubroutine(testGraph.getGraphWrapper(), 500, 10, 20000);
-//        SolutionWithStatus status = simpleGeneticSubroutine.findSolution();
-//        System.out.println(status);
-//        PartialColHeuristic heuristic = new PartialColHeuristic(testGraph);
-//        GraphSolution solution = heuristic.getColoring();
-//        System.out.println("Test");
-//        HCDHeuristic solver = new HCDHeuristic(testGraph.getGraphWrapper());
-//        GraphSolution solution = solver.getColoring();
-//        boolean satisfied = sat(testGraph, solution.getColoring());
-//        System.out.println("Done? " + satisfied);
-
-//        TabucolHeuristic heuristic = new TabucolHeuristic(testGraph);
-//        GraphSolution coloring = heuristic.getColoring();
-//        sat(testGraph, coloring.getColoring());
-//        System.out.println(coloring.getK());
-//        VertexColoringAlgorithm.Coloring<Integer> coloring = heuristic.getColoring(); // 31 colors
+        GraphOutputFormatter.writeGraphToFile(solution.getColoring(), testGraph);
 
 
 
-//        SaturationDegreeColoring<Integer, DefaultEdge> graphColoring = new SaturationDegreeColoring<>(testGraph.getGraphWrapper().getGraph()); // 38 colors
-//        VertexColoringAlgorithm.Coloring<Integer> coloring = graphColoring.getColoring();
-//        System.out.println(coloring.getNumberColors());
+
     }
 
 }
--- src/main/java/formatter/GraphOutputFormatter.java
+++ src/main/java/formatter/GraphOutputFormatter.java
@@ -5,6 +5,10 @@ import org.jgrapht.alg.interfaces.VertexColoringAlgorithm;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class GraphOutputFormatter {
     public static void writeGraphToFile(VertexColoringAlgorithm.Coloring<Integer> coloring, GraphDefinition graphDefinition) throws IOException {
@@ -18,6 +22,27 @@ public class GraphOutputFormatter {
 
         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
                 new FileOutputStream(graphDefinition.getMetadata().getGraphName() + ".soln"), StandardCharsets.UTF_8))) {
+            writer.write(schema.toString());
+        }
+    }
+
+    public static void writeGraphToFile(Map<Integer, Integer> coloring, GraphDefinition graphDefinition) throws IOException {
+
+        Schema schema = Schema.builder()
+                .vertexCount(graphDefinition.getGraphWrapper().getVertexSize())
+                .edgeCount(graphDefinition.getGraphWrapper().getGraph().edgeSet().size())
+                .colors(coloring)
+                .edges(graphDefinition.getMetadata().getEdges())
+                .build();
+
+        String outputName = graphDefinition.getMetadata().getGraphName() + ".soln";
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        String pathname = s + "\\" + "plotting" + "\\" + outputName;
+        File myFile = new File(pathname);
+        myFile.createNewFile();
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(myFile, false), StandardCharsets.UTF_8))) {
             writer.write(schema.toString());
         }
     }
